77f927226683c5e332254d75d97f49ef5217bec2
DudaGod <dyda06@mail.ru>
2015-11-13 01:02:42 +0300

    task is done

--- moment.js
+++ moment.js
@@ -1,20 +1,161 @@
 'use strict';
 
+var DAY_NAME = {
+    0: 'ВС',
+    1: 'ПН',
+    2: 'ВТ',
+    3: 'СР',
+    4: 'ЧТ',
+    5: 'ПТ',
+    6: 'СБ'
+};
+
+var DAY_VALUE = {
+    ВС: 0,
+    ПН: 1,
+    ВТ: 2,
+    СР: 3,
+    ЧТ: 4,
+    ПТ: 5,
+    СБ: 6
+};
+
+var DAYS_IN_WEEK = 7;
+
+var DAYS = {
+    value: 1000 * 60 * 60 * 24,
+    count: 30
+};
+
+var HOURS = {
+    value: 1000 * 60 * 60,
+    count: 24
+};
+
+var MINUTES = {
+    value: 1000 * 60,
+    count: 60
+};
+
+var READABLE_TIME = {
+    0: ['день', 'дня', 'дней'],
+    1: ['час', 'часа', 'часов'],
+    2: ['минута', 'минуты', 'минут']
+};
+
 module.exports = function () {
     return {
         // Здесь как-то хранится дата ;)
-        date: null,
+        _d: null,
+
+        get date() {
+            return this._d;
+        },
+
+        set date(value) {
+            if (typeof value === 'string') {
+                var parseString = parseStringToDate.bind(this);
+                value = parseString(value);
+            }
+            this._d = value;
+        },
 
         // А здесь часовой пояс
-        timezone: null,
+        _timez: null,
+
+        get timezone() {
+            return this._timez;
+        },
+
+        set timezone(value) {
+            if (this.date) {
+                var displacement = Math.max(value, this._timez) - Math.min(value, this._timez);
+                displacement *= value < this._timez ? -1 : 1;
+                this.date.setHours(this.date.getHours() + displacement, this.date.getMinutes(), 0);
+            }
+            this._timez = value;
+        },
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            if (!this.date) {
+                return 'Ограбление не состоится! Все пропало.';
+            }
+            var target = ['%DD', '%HH', '%MM'];
+            var values = {'%DD': DAY_NAME[this.date.getDay()], '%HH': this.date.getHours(),
+                '%MM': this.date.getMinutes()};
+            target.forEach(item => {
+                var pos = pattern.indexOf(item);
+                if (typeof values[item] === 'number' && values[item] < 10) {
+                    values[item] = '0' + values[item];
+                }
+                if (pos >= 0) {
+                    pattern = pattern.slice(0, pos) + values[item] +
+                        pattern.slice(-(pattern.length - pos - item.length));
+                }
+            }, this);
+            return pattern;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var mainStr = 'До ограбления ';
+            var timeRemain = [];
+            var date = this.date - moment.date;
+
+            [MINUTES, HOURS, DAYS].forEach((item, index) => {
+                if (index === 0) {
+                    timeRemain.push(Math.round(date / item.value) % item.count);
+                } else {
+                    timeRemain.push(Math.floor(date / item.value) % item.count);
+                }
+            });
+            timeRemain.reverse();
+
+            mainStr += convertToHumanReadable(timeRemain);
+            return mainStr;
         }
     };
 };
+
+function convertToHumanReadable(arrayTime) {
+    var str = '';
+    arrayTime.forEach((item, index, arr) => {
+        var num = 0;
+        if (item) {
+            if (index !== arr.length - 1 && !str) {
+                str = (/[^1]1$|^1$/.test(String(item))) ? 'остался' : 'осталось';
+            } else if (!str) {
+                str = 'осталось';
+            }
+
+            if (/[^1][2-4]$|^[2-4]$/.test(String(item))) {
+                num = 1;
+            } else if (!/[^1]1$|^1$/.test(String(item))) {
+                num = 2;
+            }
+
+            str += ' ' + item + ' ' + READABLE_TIME[index][num];
+        }
+    });
+
+    return str;
+}
+
+function parseStringToDate(string) {
+    var match = string.match(/^([А-Я]{0,2})\s?(\d{2}):(\d{2})([+-]\d{1,2})$/);
+    var date = new Date();
+    var needDay = match[1] ? DAY_VALUE[match[1]] : DAY_VALUE.ПН;
+    var currentDay = date.getDay();
+    var dist = needDay + DAYS_IN_WEEK - currentDay;
+    date.setDate(date.getDate() + dist);
+    if (this.timezone === Number(match[4]) || !this.timezone) {
+        this.timezone = Number(match[4]);
+        date.setHours(match[2], match[3], 0);
+    } else {
+        match[4] = this.timezone + (-Number(match[4]));
+        date.setHours(Number(match[2]) + match[4], match[3], 0);
+    }
+    return date;
+}
--- robbery.js
+++ robbery.js
@@ -2,22 +2,129 @@
 
 var moment = require('./moment');
 
+var DAY = {
+    ВС: 0,
+    ПН: 1,
+    ВТ: 2,
+    СР: 3,
+    ЧТ: 4,
+    ПТ: 5,
+    СБ: 6
+};
+
+var DAYS_IN_WEEK = 7;
+
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
+    appropriateMoment.timezone = 5;
 
-    // 1. Читаем json
-    // 2. Находим подходящий ближайший момент начала ограбления
-    // 3. И записываем в appropriateMoment
+    var gang = JSON.parse(json, reviever);
+    var bank = JSON.parse(JSON.stringify(workingHours), reviever);
+    var busyTime = [];
 
+    for (var prop in gang) {
+        if (!gang.hasOwnProperty(prop)) {
+            continue;
+        }
+        gang[prop].forEach(item => {
+            if (!busyTime.length) {
+                busyTime.push(item);
+            } else {
+                fillBusyTime(item);
+            }
+        });
+    }
+
+    var bankFrom = new Date(bank.from);
+    var bankTo = new Date(bank.to);
+    var appropriateTime = [];
+    busyTime.forEach(item => {
+        if (!busyTime.indexOf(item)) {
+            findAppropriateTime(item, bankFrom);
+        } else if (busyTime.indexOf(item) === busyTime.length - 1) {
+            if (item.to.getDate() !== bankTo.getDate()) {
+                bankFrom.setDate(bankFrom.getDate() + 1);
+                bankTo.setDate(bankTo.getDate() + 1);
+            }
+            findAppropriateTime(item, item.to, bankTo);
+        } else {
+            var prevItem = busyTime[busyTime.indexOf(item) - 1];
+            findAppropriateTime(item, prevItem.to);
+        }
+    });
+
+    appropriateMoment.date = appropriateTime.length ? appropriateTime[0].from : null;
     return appropriateMoment;
+
+
+
+    function findAppropriateTime(item, from, end) {
+        var to = end || item.from;
+        while (to.getDate() !== bankFrom.getDate() ||
+        from.getDate() !== bankFrom.getDate()) {
+            bankFrom.setDate(bankFrom.getDate() + 1);
+            bankTo.setDate(bankTo.getDate() + 1);
+        }
+        to = validTime(to, bankFrom, bankTo);
+        from = validTime(from, bankFrom, bankTo);
+
+        var result = to - from;
+
+        if (result / (1000 * 60) >= minDuration) {
+            appropriateTime.push({from: from, to: to});
+        }
+    }
+
+    function validTime(time, bankFrom, bankTo) {
+        return new Date(Math.min(Math.max(time, bankFrom), bankTo));
+    }
+
+    function fillBusyTime(obj) {
+        var inRange = false;
+        busyTime.forEach(item => {
+            if ((obj.from >= item.from && obj.from <= item.to) ||
+                (obj.to >= item.from && obj.to <= item.to) ||
+                (obj.from < item.from && obj.to > item.to)) {
+                item.from = new Date(Math.min(obj.from, item.from));
+                item.to = new Date(Math.max(obj.to, item.to));
+                inRange = true;
+            }
+        });
+        if (!inRange) {
+            busyTime.push(obj);
+        }
+        busyTime.sort((a, b) => a.from - b.from);
+    }
+
+    function reviever(key, value) {
+        if (key === 'from' || key === 'to') {
+            var match = value.match(/^([А-Я]{0,2})\s?(\d{2}):(\d{2})([+-]\d{1,2})$/);
+            var date = new Date();
+            var needDay = match[1] ? DAY[match[1]] : DAY.ПН;
+            var currentDay = date.getDay();
+            var dist = needDay + DAYS_IN_WEEK - currentDay;
+            date.setDate(date.getDate() + dist);
+            if (appropriateMoment.timezone === Number(match[4])) {
+                date.setHours(match[2], match[3], 0);
+            } else {
+                match[4] = appropriateMoment.timezone + (-Number(match[4]));
+                date.setHours(Number(match[2]) + match[4], match[3], 0);
+            }
+            return date;
+        }
+        return value;
+    }
 };
 
 // Возвращает статус ограбления (этот метод уже готов!)
 module.exports.getStatus = function (moment, robberyMoment) {
     if (moment.date < robberyMoment.date) {
-        // «До ограбления остался 1 день 6 часов 59 минут»
         return robberyMoment.fromMoment(moment);
+    } else if (moment.date > robberyMoment.date && robberyMoment.date) {
+        return 'Ограбление уже прошло!';
+    } else if (!robberyMoment.date) {
+        return 'Ограбление не состоится! Все пропало.';
     }
 
     return 'Ограбление уже идёт!';
